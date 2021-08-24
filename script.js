3073a84a1f0431f0a05063b9475f6ad85435de1b
RAHUL KESHRI <rkeshri2021@gmail.com>
2021-08-24 14:45:50 +0530

    Dyanamic Grid Creation Donegit add script.js

--- script.js
+++ script.js
@@ -3,48 +3,77 @@
 // color picker done!
 
 const color = document.querySelector('input[type="color"]')
-let colorValue;
+let colorValue = "#000000";
 color.addEventListener('change',(e)=>{
     colorValue = e.target.value;
 })
 
 
-
-
-const rangeEle = document.querySelector('input[type="range"]');
+// Dyanamic Grid
 const mainEle = document.querySelector('.canvas-container');
+
+// Grid Size Indicator
 const span = document.querySelectorAll('.rangeValue');
-let divChild;
 
-rangeEle.addEventListener('change', (e) => {
+// Slider
+const rangeEle = document.querySelector('input[type="range"]');
 
-    let rangeValue = e.target.value;
-    span.forEach(span => { span.textContent = rangeValue });
-    mainEle.style.cssText = `grid-template-columns:repeat(${rangeValue},1fr); grid-template-rows:repeat(${rangeValue},1fr)`;
-    let numberOfEle = rangeValue * rangeValue;
 
-    // loop 
-    for (let i = 1; i <= numberOfEle; i++) {
-        divChild = document.createElement('div');
-        divChild.addEventListener('mouseenter',e=>{
-            e.target.style.cssText=`background-color:${colorValue};`;
-        })
-        mainEle.appendChild(divChild);
+rangeEle.addEventListener('change',(e)=>{
+    let gridSize = e.target.value;
+
+
+    // display size of grid in page
+    span.forEach(span=>{
+        span.textContent = gridSize;
+    });
+
+    // defining grid size
+    mainEle.style.cssText = `grid-template-columns:repeat(${gridSize},1fr); grid-template-rows:repeat(${gridSize},1fr)`;
+
+    // removal of old elements from grid
+    const childOfMain = Array.from( mainEle.children);
+    childOfMain.forEach(child=>{
+       child.remove();
+    });
+
+    // define how many elements we want to create
+    let numOfEle = gridSize * gridSize;
+
+    // adding new elements in grid
+    for(let i = 1; i<= numOfEle; i++) {
+        let newEle = document.createElement('div');
+        newEle.addEventListener('mouseenter',e=>{
+            e.target.style.cssText = `background-color:${colorValue}`;
+        });
+        mainEle.appendChild(newEle);
     }
 
 });
 
 
-function createBasicElement() {
+
+
+
+
+
+
+
+function InitialMode() {
     const parent = document.querySelector('.canvas-container');
-    for (let i = 1; i <= (16*16); i++) {
-        let ele = document.createElement('div');
+    const sliderInitialValue = document.querySelector('input[type="range"]').value;
+    let ele;
+    console.log(sliderInitialValue);
+    for (let i = 1; i <= (sliderInitialValue * sliderInitialValue); i++) {
+        ele = document.createElement('div');
+        ele.textContent = i;
         ele.addEventListener('mouseenter',e=>{
             e.target.style.cssText=`background-color:${colorValue};`;
+            console.log(colorValue);
         })
         parent.appendChild(ele);
     }
 }
 
-window.addEventListener('load',createBasicElement);
+window.addEventListener('load',InitialMode);
 
